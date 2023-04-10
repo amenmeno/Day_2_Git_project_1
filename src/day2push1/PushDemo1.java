26948b394daa533004f473fe6ad765e46f4597f3
meno <meno@menos-mbp.hsd1.mi.comcast.net>
2023-04-10 12:52:01 -0400

    test1

--- src/day2push1/PushDemo1.java
+++ src/day2push1/PushDemo1.java
@@ -3,5 +3,14 @@ package day2push1;
 public class PushDemo1 {
 	public static void main(String[] args) {
 		System.out.println("THIS IS PROJ 1");
-}
+	}
+
+	public static void test() {
+		System.out.println("THIS IS TEST");
+	}
+	
+	public static void test2() {
+		System.out.println("THIS IS TEST");
+	}
+
 }
